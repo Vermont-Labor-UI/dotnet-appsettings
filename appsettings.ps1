2c595475cf17c654fccaa36c84439ca98028f280
Robert Cotton <Robert.Cotton@ius.gov>
2017-01-31 22:09:02 -0700

    Creating a function an doing some better logging

--- appsettings.ps1
+++ appsettings.ps1
@@ -5,8 +5,32 @@ Param(
     [string]$TimeZone = "Mountain Standard Time",
     [string]$Environment = "Development",
     [string]$LoggingSeqKey = $null,
-    [string]$LoggingSeqServerUri = $null
+    [string]$LoggingSeqServerUri = $null,
+    [bool]$VerifySqlConnectionExistance = $false
 )
+
+function CheckAppSetting
+{
+    Param([hashtable] $hash, [string] $keyName, [string] $expectedValue)
+
+    Write-Host("Verifying AppSetting $keyName")
+    if (!$hash.ContainsKey($keyName))
+    {
+        Write-Warning("$keyName is not set.  Updating to $expectedValue");
+        $hash[$keyName] = $expectedValue;
+    }
+    else
+    {
+        $existingValue = $hash[$keyName];
+        if($existingValue -ne $expectedValue)
+        {
+            Write-Warning("$keyName is not correct.  Updating from $existingValue to $expectedValue");
+            $hahs[$keyName] = $expectedValue;
+        }
+    }
+
+    return $hash;
+}
 
 Write-Host("Connecting to WebApp")
 $webApp = Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $AppName -Slot production
@@ -20,20 +44,21 @@ ForEach ($kvp in $appSettingList) {
     $hash[$kvp.Name] = $kvp.Value
 }
 
-Write-Host("Writing ClientStateName, WEBSITE_TIME_ZONE, and ASPNETCORE_ENVIRONMENT")
-$hash['ClientStateName'] = $ClientStateName
-$hash['WEBSITE_TIME_ZONE'] = $TimeZone
-$hash['ASPNETCORE_ENVIRONMENT'] = $Environment
+Write-Host("Verifying App Settings")
+$hash = CheckAppSetting($hash, 'ClientStateName', $ClientStateName)
+$hash = CheckAppSetting($hash, 'WEBSITE_TIME_ZONE', $TimeZone)
+$hash = CheckAppSetting($hash, 'ASPNETCORE_ENVIRONMENT', $Environment)
 
 if ($LoggingSeqKey) 
 {
-	$hash['Data:Logging:SeqApiKey'] = $LoggingSeqKey
+    $hash = CheckAppSetting($hash, 'Data:Logging:SeqApiKey', $LoggingSeqKey)
 }
 if ($LoggingSeqServerUri) 
 {
-	$hash['Data:Logging:SeqServerUri'] = $LoggingSeqServerUri
+    $hash = CheckAppSetting($hash, 'Data:Logging:SeqServerUri', $LoggingSeqServerUri)
 }
 
 
-Write-Host("Setting AppSettings on WebApp")
-Set-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $AppName -AppSettings $hash -Slot production
+
+#Write-Host("Setting AppSettings on WebApp")
+#Set-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $AppName -AppSettings $hash -Slot production
